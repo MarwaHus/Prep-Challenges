2e752e4d5eedc6c823c154ec1e934c441a4f0b85
MARWA H <MARWA.HU18@GMAIL.COM>
2023-06-12 15:04:10 +0000

    cc08

---
+++ code-challenges/challenges-08.js
@@ -0,0 +1,168 @@
+"use strict";
+
+// Important:
+// To solve these challenges you have use (for in ,for of) or (Object.keys ,Object.values, Object.entries )
+
+// Resources:
+// for in : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+// for of : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+
+// You are given an object that contains name and age of some customers
+// Write a function that returns an array of strings that follow the below structure
+//
+// the string format is "Customer Name :*** *** , Age :**"
+
+// Input:
+// {
+//     "Romio Joliat": 35,
+//     "Mario Ristrova": 39,
+//     "Sofia firnando": 50,
+// }
+//
+// Output:
+// ["Customer Name :Romio Joliat , Age :35", "Customer Name :Mario Ristrova , Age :39", ... ]
+
+const customerAndAge = (obj) => {
+  // write your code here
+  const customerArray = [];
+
+  for (let customer in obj) {
+    const name = customer;
+    const age = obj[customer];
+    const formattedString = `Customer Name :${name} , Age :${age}`;
+    customerArray.push(formattedString);
+  }
+
+  return customerArray;
+
+};
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02
+// Required:
+//
+// Write a function that takes recipe info object and returns an array of recipe info as strings
+// as in the example
+
+// Input:
+// let recipeInfo = {
+//   name: "pizza",
+//   ingredients: ["bread", "tomato", "chicken", "mayo"],
+//   cookTime: "twoHours",
+//   price: "25$",
+// };
+//
+// Output:
+// ["name: pizza", "ingredients: bread,tomato,chicken,mayo", "cookTime: twoHours", "price: 25$"]
+//
+// Note:
+// You can solve this challenge by using Object.entries
+
+const getEntries = (obj) => {
+  // write your code here
+  const recipeArray = [];
+
+  for (const [key, value] of Object.entries(obj)) {
+    if (key === "ingredients") {
+      const formattedString = `${key}: ${value.join()}`;
+      recipeArray.push(formattedString);
+    } else {
+      const formattedString = `${key}: ${value}`;
+      recipeArray.push(formattedString);
+    }
+  }
+
+  return recipeArray;
+
+};
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Optional:
+
+// Write a function that takes an array of objects (courses) and returns object of 2 new arrays
+// first one is containing the names of all of the courses in the data set.
+// second one is containing the names of all the students
+
+const courses = [
+  {
+    course: 'Java',
+    Instructor: 'David',
+    Students: ['Lincoln', 'Ruth', 'Briana', 'Suzy', 'Greta'],
+    GroupName: 'Stars',
+  },
+  {
+    course: 'JavaScript',
+    Instructor: 'Van',
+    Students: ['Alphonso', 'Daley', 'Dax', 'Karter', 'Jorja'],
+    GroupName: 'Nerd-ware',
+  },
+  {
+    course: 'Python',
+    Instructor: 'Delaney',
+    Students: ['Barney', 'Kalé', 'Alisha'],
+    GroupName: 'Whats-Up',
+  },
+  {
+    course: 'DotNet',
+    Instructor: 'Keanna',
+    Students: ['Oli', 'Gisselle', 'Pru'],
+    GroupName: 'Lol',
+  },
+];
+
+const getInfo = (arr) => {
+  let coursesName = [];
+  let studentsName = [];
+  // write your code here
+  for (let i = 0; i < courses.length; i++) {
+    const course = courses[i];
+    courseNames.push(course.name);
+
+    for (let j = 0; j < course.students.length; j++) {
+      const student = course.students[j];
+      if (!studentNames.includes(student)) {
+        studentNames.push(student);
+      }
+    }
+  }
+
+  return { coursesName, studentsName };
+};
+// -------------------------------------------------------------------------------------------------------
+
+//  ------------------------------------------------------------------------------------------------------
+// Challenge 04:
+// Optional:
+
+// The Head manager wants to get more organized info about some students, he wants you to find the students from the courses dataset
+// and return their info following the below structure
+//
+// Input: ['Kalé', 'Alisha','Alphonso', 'Briana']
+// Output: 
+// [
+//   {
+//     Student: 'Kalé',
+//     course: 'Python'
+//   },
+//   ...
+// ]
+
+const getStudents = (arr) => {
+  // write your code here
+};
+//  ------------------------------------------------------------------------------------------------------
+
+module.exports = {
+  customerAndAge,
+  getEntries,
+  courses,
+  getInfo,
+  getStudents,
+};
